8c993faa1643cd4b32b2ee30d44cde4f80612077
boyzwhocried <verrel.alsyoumi@gmail.com>
2023-07-27 13:43:55 +0700

    add custom animated text and icon widgets

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,71 +14,66 @@ class HomeScreen extends StatelessWidget {
     // double screenHeight = getScreenHeight(context);
 
     // bool isDark = true;
-    return Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: deviceType == DeviceType.phone ? 0 : 70,
-      ),
-      child: Scaffold(
-        endDrawer: const MainDrawer(),
-        appBar: AppBar(
-          toolbarHeight: 70,
-          backgroundColor:
-              Theme.of(context).colorScheme.inversePrimary.withOpacity(0.1),
-          title: const Text(
-            'boyzwhocried',
-            style: TextStyle(
-              letterSpacing: 2.0,
-              fontWeight: FontWeight.bold,
-            ),
+    return Scaffold(
+      endDrawer: const MainDrawer(),
+      appBar: AppBar(
+        toolbarHeight: 70,
+        backgroundColor:
+            Theme.of(context).colorScheme.inversePrimary.withOpacity(0.1),
+        title: const Text(
+          'boyzwhocried',
+          style: TextStyle(
+            letterSpacing: 2.0,
+            fontWeight: FontWeight.bold,
           ),
-          centerTitle: deviceType == DeviceType.phone,
-          actions: [
-            if (deviceType != DeviceType.phone)
-              Padding(
-                padding: const EdgeInsets.all(3.0),
-                child: Row(
-                  children: [
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'explore',
-                        style: TextStyle(
-                          letterSpacing: 3.0,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'about',
-                        style: TextStyle(
-                          letterSpacing: 3.0,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
-                    Consumer<DarkModeProvider>(
-                      builder: (context, darkModeProvider, child) {
-                        return IconButton(
-                          onPressed: () {
-                            darkModeProvider.toggleDarkMode();
-                          },
-                          icon: Icon(
-                            darkModeProvider.isDarkMode
-                                ? Icons.dark_mode
-                                : Icons.light_mode,
-                          ),
-                        );
-                      },
-                    ),
-                  ],
-                ),
-              ),
-          ],
         ),
-        // body: ,
+        centerTitle: deviceType == DeviceType.phone,
+        actions: [
+          if (deviceType != DeviceType.phone)
+            Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Row(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'explore',
+                      style: TextStyle(
+                        letterSpacing: 3.0,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text(
+                      'about',
+                      style: TextStyle(
+                        letterSpacing: 3.0,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                  Consumer<DarkModeProvider>(
+                    builder: (context, darkModeProvider, child) {
+                      return IconButton(
+                        onPressed: () {
+                          darkModeProvider.toggleDarkMode();
+                        },
+                        icon: Icon(
+                          darkModeProvider.isDarkMode
+                              ? Icons.dark_mode
+                              : Icons.light_mode,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ),
+        ],
       ),
+      // body: ,
     );
   }
 }
